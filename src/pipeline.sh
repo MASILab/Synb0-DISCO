8d2dc20d077b3e12b76923ca5547b86f42990626
leonyichencai <leonyichencai@gmail.com>
2023-11-02 11:03:18 -0500

    new topup config

--- src/pipeline.sh
+++ src/pipeline.sh
@@ -61,7 +61,7 @@ if [[ $TOPUP -eq 1 ]]; then
     # Merge results and run through topup
     echo Running topup
     fslmerge -t /OUTPUTS/b0_all.nii.gz /OUTPUTS/b0_d_smooth.nii.gz /OUTPUTS/b0_u.nii.gz
-    topup -v --imain=/OUTPUTS/b0_all.nii.gz --datain=/INPUTS/acqparams.txt --config=b02b0.cnf --iout=/OUTPUTS/b0_all_topup.nii.gz --out=/OUTPUTS/topup --subsamp=1,1,1,1,1,1,1,1,1 --miter=10,10,10,10,10,20,20,30,30 --lambda=0.00033,0.000067,0.0000067,0.000001,0.00000033,0.000000033,0.0000000033,0.000000000033,0.00000000000067 --scale=0
+    topup -v --imain=/OUTPUTS/b0_all.nii.gz --datain=/INPUTS/acqparams.txt --config=/extra/synb0.cnf --iout=/OUTPUTS/b0_all_topup.nii.gz --out=/OUTPUTS/topup
 fi
 
 
